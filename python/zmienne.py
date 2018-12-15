d87f18be5f90f40630bb7bd9bbb0641116228b14
Klemens Noga <klemens.noga@gmail.com>
2018-12-15 16:24:55 +0100

    Skrypty zmienne oraz wyszukiwanie slow

---
+++ python/zmienne.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+zmienna = "Jest sobota i uczymy sie Pythona"
+print("Zmienna 'zmiena' jest typu", type(zmienna), "i zawiera:'",
+       zmienna,"'.", sep = "_",end = '\n kabooom!!!\n')
+
+zmienna = 35//5
+
+print("Zmienna 'zmiena' jest typu", type(zmienna), "i zawiera:'",
+       zmienna,"'.", sep = "_",end = '\n kabooom!!!\n')
+
+zmienna = zmienna/387
+
+print("Zmienna 'zmiena' jest typu", type(zmienna), "i zawiera:'",
+       zmienna,"'.", sep = "_",end = '\n kabooom!!!\n')
+
+liczba = 60
+print("Zmienna 'liczba' jest typu", type(liczba), "i zawiera:'",
+       liczba,"'.", sep = "_",end = '\n kabooom!!!\n')
+
+zmienna = zmienna + liczba
+
+print("Zmienna 'zmiena' jest typu", type(zmienna), "i zawiera:'",
+       zmienna,"'.", sep = "_",end = '\n kabooom!!!\n')
+
+napis = "To jest zmienna znakowa tj. string"
+
+print("Zmienna 'napis' jest typu", type(napis), "i zawiera:'",
+       napis,"'.", sep = "_",end = '\n kabooom!!!\n')
+
+zmienna = str(zmienna) + " " + napis
+
+print("Zmienna 'zmiena' jest typu", type(zmienna), "i zawiera:'",
+       zmienna,"'.", sep = "_",end = '\n kabooom!!!\n')
+
+
+
